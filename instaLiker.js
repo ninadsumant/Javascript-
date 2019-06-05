298d6901dfbc60825176029011c7b447ff84f42d
ninadsumant <ninadssumant@gmail.com>
2019-06-05 23:44:37 +0530

    Instagram automation for likes

---
+++ instaLiker.js
@@ -0,0 +1,11 @@
+let likesGiven = 0;
+setInterval(() => {
+    let heart = document.getElementsByClassName('glyphsSpriteHeart__outline__24__grey_9'),
+          arrow = document.querySelector('.coreSpriteRightPaginationArrow');
+    if (heart[1]) {
+        heart = heart[1].parentElement;
+        likesGiven++, heart.click();
+    }
+    arrow.click();
+    console.log(`You've liked ${likesGiven} post(s)!`);
+}, 2000);
